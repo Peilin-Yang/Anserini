c755b010cee7014f9d00f1be1f18e53736fda71a
Peilin Yang <peiliny@twitter.com>
2018-07-05 17:54:06 -0700

    update

--- simple_searcher_test.py
+++ simple_searcher_test.py
@@ -1,5 +1,5 @@
 import jnius_config
-jnius_config.set_classpath("target/anserini-0.0.1-SNAPSHOT-fatjar.jar")
+jnius_config.set_classpath("target/anserini-0.1.1-SNAPSHOT-fatjar.jar")
 
 from jnius import autoclass
 JSearcher = autoclass('io.anserini.search.SimpleSearcher')
